1f076f72c1b98247a9c81e185d0826d2e38b56a6
olawale-oloye <olawaleoloye@gmail.com>
2023-09-08 08:09:09 +1000

    update to files

--- assests/js/main.js
+++ assests/js/main.js
@@ -15,41 +15,15 @@ const navList = document.querySelector("nav ul");
 
 // To display Day of the Week
 const currentDateValue = days[new Date().getDay()];
-document.querySelector(
-  ".current-day"
-).textContent = `Today is ${currentDateValue}`;
+document.querySelector(".current-day").textContent = currentDateValue;
 
 // To display the current UTC time
 const currentUTCTImeValue = new Date(
   new Date().getTime() - new Date().getTimezoneOffset() * 60000
 )
   .toISOString()
-  .toString()
-  .slice(11, 19);
+  .toString();
 
-document.querySelector(
-  ".current-time"
-).textContent = `Current UTC Time is ${currentUTCTImeValue}`;
+document.querySelector(".current-time").textContent = currentUTCTImeValue;
 
-// Toggler
-openNav.addEventListener("click", function () {
-  navList.style.top = "17%";
-  openNav.style.display = "none";
-  closeNav.style.display = "block";
-});
-
-function closeNavToggler() {
-  navList.style.top = "-70%";
-  openNav.style.display = "block";
-  closeNav.style.display = "none";
-}
-
-closeNav.addEventListener("click", function () {
-  closeNavToggler();
-});
-
-document.querySelectorAll("nav ul li a").forEach((navList) => {
-  navList.addEventListener("click", function () {
-    closeNavToggler();
-  });
-});
+// .slice(11, 19)
